b85a4e2488fcf558ae3d3c8eeefc64437eb22811
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-05-18 21:00:11 +0800

    配置阿里云Oss存储

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,28 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建AliOssUtil对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean // 保证只有这一个util对象,虽然spring，默认是单例模式，但是如果存在其他配置类，其他配置类中也创建了oss对象，那么容器中仍然会存在多个
+
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
+        log.info("开始创建阿里云文件上传工具类对象,{}",aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
