64458a3fe56d273bfe20ac839db7c52d449ec522
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-05-19 08:52:40 +0800

    文件上传到oss接口

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,23 +1,33 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 通用接口
  */
-@Controller
+@RestController
 @RequestMapping("/admin/common")
 @Api(tags = "通用接口")
 @Slf4j
 public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
     /**
      * 文件上传
@@ -27,9 +37,24 @@ public class CommonController {
     @PostMapping("/upload")
     @ApiOperation("文件上传")
     public Result<String> upload(MultipartFile file){
-      log.info("文件上传: {}",file);
+        log.info("文件上传: {}",file);
 
+        //防止文件重名，所以要对文件重命名
+        try {
+            //原始文件名
+            String originalFilename = file.getOriginalFilename();
+            //截取原始文件名的后缀
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //新的文件名
+            String objectName = UUID.randomUUID().toString() + extension;
 
-      return null;
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+
+            return Result.success(filePath);
+        } catch (Exception e) {
+            log.error("文件上传失败,{}",e.toString());
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,2 @@
+package com.sky.controller.admin;public class DishController {
+}
