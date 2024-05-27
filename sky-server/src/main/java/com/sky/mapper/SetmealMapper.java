b931536b30dbdf2d4b24c4157ed33e1f392f1403
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-05-27 22:57:48 +0800

    工作台代码

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -85,4 +86,12 @@ public interface SetmealMapper {
     List<DishItemVO> getDishItemBySetmealId(String setmealId);
 
     Integer updateSetmealStatusStopByIds(List<String> ids);
+
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
+
 }
